d92aae57f6220a6e831900db26f66b369f166332
katherynmk <katherynmknutson@lewisu.edu>
2022-02-01 22:27:29 -0600

    help

--- exp.js
+++ exp.js
@@ -6,8 +6,10 @@ function testInput(re, str) {
   let midstring;
   if (re.test(str)) {
     midstring = 'contains';
+    return 'true'
   } else {
     midstring = 'does not contain';
+    return 'false'
   }
   console.log(`${str} ${midstring} ${re.source}`);
 }
