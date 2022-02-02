23c394bb6c60ee531c5ddb0a34be75de606c85f3
katherynmk <katherynmknutson@lewisu.edu>
2022-02-01 22:24:03 -0600

    not good

--- exp.js
+++ exp.js
@@ -1,39 +1,13 @@
-function RollSixSidedDice() {
-  //this just rolls dice for both boxes from the one button
-  document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
+re = document.getElementById("testleft")
+str = document.getElementById("testright")
+
+function testInput(re, str) {
   
-  document.getElementById("dice1").value = Math.floor(Math.random()*6) + 1;
-  
-
-}
-//this is the function that changes text + gives confetti for wins and ties
-
-function Winner(){
-  let header = document.querySelector("p");
-var x = document.getElementById("dice").value;
-var y =  document.getElementById("dice1").value;
-
-if (x > y) {
-  confetti();
-  confetti({
-  spread: 100
-  });
-  
-  header.innerText = "Player 1 Wins";
-}
-if(x < y) {
-  confetti();
-  confetti({
-  spread: 100
-  });
-  header.innerText = "Player 2 Wins";
-
-}
-
-if (x == y){
-  header.innerText = "Tie";
-
-}
-
-
+  let midstring;
+  if (re.test(str)) {
+    midstring = 'contains';
+  } else {
+    midstring = 'does not contain';
+  }
+  console.log(`${str} ${midstring} ${re.source}`);
 }
