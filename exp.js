9f1c23be0dbb1fba18d19687d24f6c27d1d273c7
katherynmk <katherynmknutson@lewisu.edu>
2022-02-02 18:24:39 -0600

    help

--- exp.js
+++ exp.js
@@ -1,15 +1,19 @@
-re = document.getElementById("testleft")
-str = document.getElementById("testright")
 
-function testInput(re, str) {
-  
+
+function testInput() {
+  var re = document.getElementById("testleft").innerHTML
+  var str = document.getElementById("testright").innerHTML
+
   let midstring;
   if (re.test(str)) {
     midstring = 'contains';
+    console.log('true');
     return 'true'
   } else {
     midstring = 'does not contain';
+    console.log('false');
     return 'false'
   }
-  console.log(`${str} ${midstring} ${re.source}`);
+  
+  
 }
