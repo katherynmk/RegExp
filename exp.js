bd66f7c7b12747481a176dde4333972fc6f7adb1
katherynmk <katherynmknutson@lewisu.edu>
2022-02-02 20:55:06 -0600

    done

--- exp.js
+++ exp.js
@@ -1,12 +1,19 @@
 
 
 function testInput() {
-
+  let header = document.querySelector("p");
   var re = document.getElementById("testleft").value;
   var str = document.getElementById("testright").value;
   var regex = new RegExp(re);
+  
   console.log(regex.test(str));
 
-  
+if (regex.test(str)){
+  header.innerText = "True";
+}  
+
+else{
+  header.innerText = "False";
+}
   
 }
